7b574f01c40369f2235527184e76d73220fded5a
Tvsmadhur <tvsmadhur09@gmail.com>
2021-06-21 12:56:50 +0530

    timer app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  // write your code here
+  const [time, setTime] = useState(0);
+  const [valid, setValid] = useState(false);
+
+  useEffect(() => {
+    !valid &&
+      setTimeout(() => {
+        time > 0 && setTime(time - 1);
+      }, 1000);
+    valid && setTime(0);
+  }, [time]);
+
+  function decreamentCount(e) {
+    if (e.key === "Enter") {
+      // console.log(isNaN(e.target.value));
+      if (isNaN(e.target.value) || e.target.value == 0) {
+        setValid(true);
+      } else {
+        setValid(false);
+        setTime(Math.floor(e.target.value));
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for
+          <input
+            id="timeCount"
+            onKeyDown={decreamentCount /* callback function */}
+          />{" "}
+          sec.
+        </h1>
+      </div>
+      <div id="current-time">{valid ? 0 : time /* remaining time */}</div>
     </div>
   );
-}
+};
 
 export default App;
